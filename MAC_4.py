9f5cde856857a2a88310ff0a773184bc1c510eed
dougBelcher <28TSRBUED4@I%SQC7P%p>
2021-06-29 12:06:19 -0500

    Create a snapshot of current image

--- MAC_4.py
+++ MAC_4.py
@@ -4,6 +4,7 @@ import sys
 import pandas as pd
 from dotenv import DotEnv
 import sqlite3
+from pprint import pprint
 
 os.chdir('C:\\Users\\WRA1523\\Dropbox\\Yellow Folders\\Python')           # work
 # os.chdir('C:\\Users\\Doug\\Documents\\Dropbox\\Yellow Folders\\Python')   # home PC
@@ -18,15 +19,38 @@ os.chdir('\\Users\\WRA1523\\')                                              # wo
 conn = sqlite3.connect('MAC_data.db')
 c = conn.cursor()
 
-c.execute("""CREATE TABLE MAC (
-        MAC_ID text,
-        SN text,
-        F_name text
-        )""")
+try:
+    c.execute("""CREATE TABLE MAC (
+            MACADDRESS text,
+            SERIALNUMBER text,
+            DATECODE text,
+            TESTDATE text,
+            file_name text
+            )""")
+except:
+    pass
 
-sys.exit()
+# Delete Records
+c.execute("""DELETE FROM MAC""")
 
-# os.chdir('//wrprod/wrdsup/supdata')                                         # work
+conn.commit()
+
+# Set up for many ID insert
+# many_SN = [('346F921B4C04', '710482321278C6', '6/7/2021', '6/7/2021', '20210609123119292imics1.csv'),
+#            ('346F921B4C0E', '710482321278D0', '6/7/2021', '6/7/2021', '20210609123119292imics1.csv'),
+#            ('346F921B4C0F', '710482321278D1', '6/7/2021', '6/7/2021', '20210609123119292imics1.csv'),
+#            ('346F921B4C14', '710482321278D6', '6/7/2021', '6/7/2021', '20210609123119292imics1.csv')]
+
+# c.executemany("INSERT INTO MAC VALUES(?, ?, ?, ?, ?)", many_SN)
+
+# Query the Database
+# c.execute("SELECT rowid, * FROM MAC")
+
+# pprint(f'{c.fetchall()}')
+
+# sys.exit()
+
+os.chdir('//wrprod/wrdsup/supdata')                                         # work
 # os.chdir('//wrprod/wrdsup/supdata')                                         # ubuntu
 # os.chdir('//wrprod/wrdsup/supdata')                                         # home PC
 
@@ -34,18 +58,15 @@ directory = os.path.join('//wrprod/wrdsup/supdata')
 for root, dirs, files in os.walk(directory):
     for file in files:
         if file.endswith(".csv"):
-            f = open(file, 'r')
             try:
+                f = open(file, 'r')
+                print(f'got this far: 1')
                 df = pd.read_csv(file)
+                print(f'got this far: 2')
                 if df.size > 0:
-                    # if hdr_parm:
-                    #     df["file_name"] = file
-                    #     df.to_csv(file1, index=False, header=hdr_parm, line_terminator='\n')
-                    #     hdr_parm = False
-                    # else:
-                    #     df["file_name"] = file
-                    #     df.to_csv(file1, index=False, header=hdr_parm, line_terminator='\n')
-                    pass
+                    print(f'got this far: 3')
+                    # c.execute("INSERT INTO customers VALUES('John', 'Elder', 'john@codemy.com')")
+                    df.to_sql(MAC, c, index=False, if_exists='append')
                 else:
                     print(f"Empty Data File: {file}")
                 f.close()
