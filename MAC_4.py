b8e52fd5ebdc1026fcb3ae666481cff5525d7958
dougBelcher <ddbnull+git@gmail.com>
2021-06-29 19:55:38 -0500

    Update the latest MAC script

--- MAC_4.py
+++ MAC_4.py
@@ -21,10 +21,10 @@ c = conn.cursor()
 
 try:
     c.execute("""CREATE TABLE MAC (
-            MACADDRESS text,
-            SERIALNUMBER text,
-            DATECODE text,
-            TESTDATE text,
+            MACaddress text,
+            SerialNumber text,
+            BuildDate text,
+            TestDate text,
             file_name text
             )""")
 except:
@@ -34,21 +34,6 @@ except:
 c.execute("""DELETE FROM MAC""")
 
 conn.commit()
-
-# Set up for many ID insert
-# many_SN = [('346F921B4C04', '710482321278C6', '6/7/2021', '6/7/2021', '20210609123119292imics1.csv'),
-#            ('346F921B4C0E', '710482321278D0', '6/7/2021', '6/7/2021', '20210609123119292imics1.csv'),
-#            ('346F921B4C0F', '710482321278D1', '6/7/2021', '6/7/2021', '20210609123119292imics1.csv'),
-#            ('346F921B4C14', '710482321278D6', '6/7/2021', '6/7/2021', '20210609123119292imics1.csv')]
-
-# c.executemany("INSERT INTO MAC VALUES(?, ?, ?, ?, ?)", many_SN)
-
-# Query the Database
-# c.execute("SELECT rowid, * FROM MAC")
-
-# pprint(f'{c.fetchall()}')
-
-# sys.exit()
 
 os.chdir('//wrprod/wrdsup/supdata')                                         # work
 # os.chdir('//wrprod/wrdsup/supdata')                                         # ubuntu
@@ -60,18 +45,24 @@ for root, dirs, files in os.walk(directory):
         if file.endswith(".csv"):
             try:
                 f = open(file, 'r')
-                print(f'got this far: 1')
                 df = pd.read_csv(file)
-                print(f'got this far: 2')
                 if df.size > 0:
-                    print(f'got this far: 3')
                     # c.execute("INSERT INTO customers VALUES('John', 'Elder', 'john@codemy.com')")
-                    df.to_sql(MAC, c, index=False, if_exists='append')
+                    # print(f'{df.head()}')
+                    df.to_sql('MAC', c, index=False, if_exists='append')
                 else:
                     print(f"Empty Data File: {file}")
                 f.close()
+            except ValueError:
+                print(f'{df.head}')
             except:
                 print(f"Can't access file : {file}")
 
+# Query the Database
+# c.execute("SELECT rowid, * FROM MAC")
+
+# pprint(f'{c.fetchall()}')
+
+# sys.exit()
 print('done')
 file1.close()
