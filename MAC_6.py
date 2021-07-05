159096f016fc491a714387ec4b720b724b625475
Doug Belcher <ddbnull@gmail.com>
2021-07-05 18:22:18 -0500

    Update MAC scripts

--- MAC_6.py
+++ MAC_6.py
@@ -4,20 +4,19 @@
 #   TODO - Add SQLite output to this *edit* change to csvs-to-sqlite
 import os
 import sys
-
 import pandas as pd
 from datetime import datetime
 
 print(f'start: {datetime.now()}')
 # os.chdir('\\Users\\WRA1523\\')                    # work
 os.chdir('/home/doug/Downloads')                    # ubuntu
+# sys.exit()
 MAC_file = open("MACSensi(0).csv", "w+")
 hdr_parm = True
-# sys.exit()
 
-# os.chdir('//wrprod/wrdsup/supdata')
+# os.chdir('//wrprod/wrdsup/supdata')                                           # work
+# directory = os.path.join('//wrprod/wrdsup/supdata')                           # work
 os.chdir('/home/doug/Dropbox/Yellow Folders/Python/WR')                        # ubuntu
-# directory = os.path.join('//wrprod/wrdsup/supdata')
 directory = os.path.join('/home/doug/Dropbox/Yellow Folders/Python/WR')        # ubuntu
 for root, dirs, files in os.walk(directory):
     for file in files:
@@ -26,19 +25,19 @@ for root, dirs, files in os.walk(directory):
                 print('Step 1')
                 f = open(file, 'r')
                 print('Step 2')
-                print(f'{directory}')
+                # print(f'{directory}')
                 df = pd.read_csv(file)
                 print('Step 3')
                 if df.size > 0:
                     # df["file_name"] = file
-                    print(f'Got this far 4: {file}')
+                    print(f'Step 4: {file}')
                     if hdr_parm:
                         df.to_csv(MAC_file, index=False, header=hdr_parm, line_terminator='\n',
-                                  columns='MACaddress,SerialNumber,BuildDate,TestDate, file_name')
+                                  columns='MACaddress, SerialNumber, BuildDate, TestDate, file_name')
                         hdr_parm = False
                     else:
                         df.to_csv(MAC_file, index=False, header=hdr_parm, line_terminator='\n',
-                                  columns='MACaddress,SerialNumber,BuildDate,TestDate, file_name')
+                                  columns='MACaddress, SerialNumber, BuildDate, TestDate, file_name')
                 else:
                     print(f"Empty Data File: {file}")
                 f.close()
