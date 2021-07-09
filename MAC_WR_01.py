e1c741931962a0f51b923dc9792d1625b4efde18
Doug Belcher <ddbnull@gmail.com>
2021-07-09 15:17:29 -0500

    Update MAC_WR_01.py

--- MAC_WR_01.py
+++ MAC_WR_01.py
@@ -14,20 +14,18 @@ pd.options.mode.chained_assignment = None  # default='warn'
 print(f'start: {datetime.now()}')
 str_time = time.time()
 
-# os.chdir('\\Users\\WRA1523\\')
 os.chdir('\\Users\\WRA1523\\OneDrive - Emerson\\Varsity\\Python')
 MAC_file = open("MACSensi.csv", "w+")
-hdr_parm = True
 
 conn = sqlite3.connect('MAC_data.db')
 c = conn.cursor()
 
 try:
     c.execute("""CREATE TABLE MAC (
-            MAC text PRIMARY KEY,
-            SERIAL text,
-            DATE1 text,
-            DATE2 text,
+            MACaddress text,
+            SerialNumber text,
+            BuildDate text,
+            TestDate text,
             file_name text
             )""")
     conn.commit()
@@ -36,14 +34,8 @@ except:
 
 # Delete Records
 c.execute("""DELETE FROM MAC""")
-
 conn.commit()
 
-# for row in c.execute('SELECT * FROM MAC'):
-#     print(f'{row}')
-
-# os.chdir('\\Users\\WRA1523\\OneDrive - Emerson\\Emerson\\supdata')
-# directory = os.path.join('\\Users\\WRA1523\\OneDrive - Emerson\\Emerson\\supdata')
 os.chdir('//wrprod/wrdsup/supdata')
 directory = os.path.join('//wrprod/wrdsup/supdata')
 for root, dirs, files in os.walk(directory):
@@ -56,22 +48,15 @@ for root, dirs, files in os.walk(directory):
                 if modDF.size > 0:
                     modDF.dropna(axis=0, how='all', inplace=True)
                     modDF["file_name"] = file
-                    if hdr_parm:
-                        # modDF.to_csv(MAC_file, index=False, header=hdr_parm, line_terminator='\n')
-                        # print(f'{modDF.head()}')
-                        modDF.to_sql('MAC', conn, index=False, if_exists='append')
-                        hdr_parm = False
-                    else:
-                        # modDF.to_csv(MAC_file, index=False, header=hdr_parm, line_terminator='\n')
-                        modDF.to_sql('MAC', conn, index=False, if_exists='append')
+                    modDF.to_sql('MAC', conn, index=False, if_exists='append')
                 else:
                     print(f"Empty Data File: {file} - {list(df.columns)}")
                 f.close()
-            except PermissionError:
-                print(f"Can't access file: {file}")
+            except PermissionError as e:
+                print(f"Can't access file: {file} : {e}")
             except:
-                # print(list(df.columns))
-                print(f"Unknown error: {file} - {list(df.columns)}")
+                e = sys.exc_info()
+                print(f"Unknown error: {file}: {e}")
 
 print(f'done: {datetime.now()} - {(time.time() - str_time)/60}')
 MAC_file.close()
